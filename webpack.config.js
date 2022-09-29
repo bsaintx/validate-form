f1a7009ac08929021fd94e7bcf8b86623297e21e
Bruno Coutinho <111131598+bsaintx@users.noreply.github.com>
2022-09-29 15:45:29 -0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/A19-exercicio/A19-exercicio.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.frontend.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
+  },
+  devtool: 'source-map',
+};
